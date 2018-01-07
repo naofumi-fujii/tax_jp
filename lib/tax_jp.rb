6bb5d35a11c8d665efec9d8d0ab3f8c9a5213bd5
naofumi-fujii <naofumi.fujii@gmail.com>
2018-01-07 19:41:17 +0900

    refactor

--- lib/tax_jp.rb
+++ lib/tax_jp.rb
@@ -42,34 +42,65 @@ module TaxJp
       end
     end
 
-    def income_tax
+    def deductions
       # 195万円以下	5％	0円
       if value <= 195 * 10000
-        return value - value * tax_rate + 0
+        return 0
       end
       # 195万円を超え 330万円以下	10％	97,500円
       if (1950001..3300000).include? value
-        return value - (value * tax_rate) + 97500
+        return 97500
       end
       # 330万円を超え 695万円以下	20％	427,500円
       if (3300001..6950000).include? value
-        return value - (value * tax_rate) + 427500
+        return 427500
       end
       # 695万円を超え 900万円以下	23％	636,000円
       if (6950001..9000000).include? value
-        return value - (value * tax_rate) + 636000
+        return 636000
       end
       # 900万円を超え 1,800万円以下	33％	1,536,000円
       if (9000001..18000000).include? value
-        return value - (value * tax_rate) + 1536000
+        return 1536000
       end
       # 1,800万円を超え4,000万円以下	40％	2,796,000円
       if (18000000..40000000).include? value
-        return value - (value * tax_rate) + 2796000
+        return 2796000
       end
       # 4,000万円超	45％	4,796,000円
       if 40000000 < value
-        return value - (value * tax_rate) + 4796000
+        return 4796000
+      end
+    end
+
+    def income_tax
+      # 195万円以下	5％	0円
+      if value <= 195 * 10000
+        return value - value * tax_rate + deductions
+      end
+      # 195万円を超え 330万円以下	10％	97,500円
+      if (1950001..3300000).include? value
+        return value - (value * tax_rate) + deductions
+      end
+      # 330万円を超え 695万円以下	20％	427,500円
+      if (3300001..6950000).include? value
+        return value - (value * tax_rate) + deductions
+      end
+      # 695万円を超え 900万円以下	23％	636,000円
+      if (6950001..9000000).include? value
+        return value - (value * tax_rate) + deductions
+      end
+      # 900万円を超え 1,800万円以下	33％	1,536,000円
+      if (9000001..18000000).include? value
+        return value - (value * tax_rate) + deductions
+      end
+      # 1,800万円を超え4,000万円以下	40％	2,796,000円
+      if (18000000..40000000).include? value
+        return value - (value * tax_rate) + deductions
+      end
+      # 4,000万円超	45％	4,796,000円
+      if 40000000 < value
+        return value - (value * tax_rate) + deductions
       end
     end
   end
