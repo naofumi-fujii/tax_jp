2f7a5547d1c21f24e5752a06ae8ad37cca53134e
naofumi-fujii <naofumi.fujii@gmail.com>
2018-01-07 19:43:16 +0900

    $ bundle exec rubocop -a lib/tax_jp.rb

--- lib/tax_jp.rb
+++ lib/tax_jp.rb
@@ -1,107 +1,73 @@
-require "tax_jp/version"
+require 'tax_jp/version'
 
 module TaxJp
   # Your code goes here...
   class Income
-    def initialize value
+    def initialize(value)
       @value = value
     end
 
-    def value
-      @value
-    end
+    attr_reader :value
 
     def tax_rate
       # 195万円以下	5％	0円
-      if value <= 195 * 10000
-        return  0.05
-      end
+      return 0.05 if value <= 195 * 10_000
       # 195万円を超え 330万円以下	10％	97,500円
-      if (1950001..3300000).include? value
-        return 0.10
-      end
+      return 0.10 if (1_950_001..3_300_000).cover? value
       # 330万円を超え 695万円以下	20％	427,500円
-      if (3300001..6950000).include? value
-        return 0.20
-      end
+      return 0.20 if (3_300_001..6_950_000).cover? value
       # 695万円を超え 900万円以下	23％	636,000円
-      if (6950001..9000000).include? value
-        return 0.23
-      end
+      return 0.23 if (6_950_001..9_000_000).cover? value
       # 900万円を超え 1,800万円以下	33％	1,536,000円
-      if (9000001..18000000).include? value
-        return 0.33
-      end
+      return 0.33 if (9_000_001..18_000_000).cover? value
       # 1,800万円を超え4,000万円以下	40％	2,796,000円
-      if (18000000..40000000).include? value
-        return 0.40
-      end
+      return 0.40 if (18_000_000..40_000_000).cover? value
       # 4,000万円超	45％	4,796,000円
-      if 40000000 < value
-        return 0.45
-      end
+      return 0.45 if value > 40_000_000
     end
 
     def deductions
       # 195万円以下	5％	0円
-      if value <= 195 * 10000
-        return 0
-      end
+      return 0 if value <= 195 * 10_000
       # 195万円を超え 330万円以下	10％	97,500円
-      if (1950001..3300000).include? value
-        return 97500
-      end
+      return 97_500 if (1_950_001..3_300_000).cover? value
       # 330万円を超え 695万円以下	20％	427,500円
-      if (3300001..6950000).include? value
-        return 427500
-      end
+      return 427_500 if (3_300_001..6_950_000).cover? value
       # 695万円を超え 900万円以下	23％	636,000円
-      if (6950001..9000000).include? value
-        return 636000
-      end
+      return 636_000 if (6_950_001..9_000_000).cover? value
       # 900万円を超え 1,800万円以下	33％	1,536,000円
-      if (9000001..18000000).include? value
-        return 1536000
-      end
+      return 1_536_000 if (9_000_001..18_000_000).cover? value
       # 1,800万円を超え4,000万円以下	40％	2,796,000円
-      if (18000000..40000000).include? value
-        return 2796000
-      end
+      return 2_796_000 if (18_000_000..40_000_000).cover? value
       # 4,000万円超	45％	4,796,000円
-      if 40000000 < value
-        return 4796000
-      end
+      return 4_796_000 if value > 40_000_000
     end
 
     def income_tax
       # 195万円以下	5％	0円
-      if value <= 195 * 10000
-        return value - value * tax_rate + deductions
-      end
+      return value - value * tax_rate + deductions if value <= 195 * 10_000
       # 195万円を超え 330万円以下	10％	97,500円
-      if (1950001..3300000).include? value
+      if (1_950_001..3_300_000).cover? value
         return value - (value * tax_rate) + deductions
       end
       # 330万円を超え 695万円以下	20％	427,500円
-      if (3300001..6950000).include? value
+      if (3_300_001..6_950_000).cover? value
         return value - (value * tax_rate) + deductions
       end
       # 695万円を超え 900万円以下	23％	636,000円
-      if (6950001..9000000).include? value
+      if (6_950_001..9_000_000).cover? value
         return value - (value * tax_rate) + deductions
       end
       # 900万円を超え 1,800万円以下	33％	1,536,000円
-      if (9000001..18000000).include? value
+      if (9_000_001..18_000_000).cover? value
         return value - (value * tax_rate) + deductions
       end
       # 1,800万円を超え4,000万円以下	40％	2,796,000円
-      if (18000000..40000000).include? value
+      if (18_000_000..40_000_000).cover? value
         return value - (value * tax_rate) + deductions
       end
       # 4,000万円超	45％	4,796,000円
-      if 40000000 < value
-        return value - (value * tax_rate) + deductions
-      end
+      return value - (value * tax_rate) + deductions if value > 40_000_000
     end
   end
 end
