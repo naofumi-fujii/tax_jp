e9571486e4ad01a3c20f73cf0f3d2441fe0a764c
naofumi-fujii <naofumi.fujii@gmail.com>
2018-01-07 20:10:50 +0900

    update spec

--- spec/tax_jp_spec.rb
+++ spec/tax_jp_spec.rb
@@ -3,28 +3,26 @@ RSpec.describe TaxJp do
     expect(TaxJp::VERSION).not_to be nil
   end
 
-  let(:test_data) do
-    [
-      [195 * 10000, 0.05, 0],
+  test_data = [
+    [195 * 10000, 0.05, 0],
 
-      [195 * 10000 + 1, 0.10, 97500],
-      [330 * 10000, 0.10, 97500],
+    [195 * 10000 + 1, 0.10, 97500],
+    [330 * 10000, 0.10, 97500],
 
-      [330 * 10000 + 1, 0.20, 427500],
-      [695 * 10000, 0.20, 427500],
+    [330 * 10000 + 1, 0.20, 427500],
+    [695 * 10000, 0.20, 427500],
 
-      [695 * 10000 + 1, 0.23, 636000],
-      [900 * 10000, 0.23, 636000],
+    [695 * 10000 + 1, 0.23, 636000],
+    [900 * 10000, 0.23, 636000],
 
-      [900 * 10000 + 1, 0.33, 1536000],
-      [1800 * 10000, 0.33, 1536000],
+    [900 * 10000 + 1, 0.33, 1536000],
+    [1800 * 10000, 0.33, 1536000],
 
-      [1800 * 10000 + 1, 0.40, 2796000],
-      [4000 * 10000, 0.40, 2796000],
+    [1800 * 10000 + 1, 0.40, 2796000],
+    [4000 * 10000, 0.40, 2796000],
 
-      [4000 * 10000 + 1, 0.45, 4796000],
-    ]
-  end
+    [4000 * 10000 + 1, 0.45, 4796000],
+  ]
 
   # https://www.nta.go.jp/taxanswer/shotoku/2260.htm
   # 所得税の速算表
@@ -37,12 +35,28 @@ RSpec.describe TaxJp do
   # 1,800万円を超え4,000万円以下	40％	2,796,000円
   # 4,000万円超	45％	4,796,000円
 
-  example do
-    test_data.each do |value, tax_rate, deductions|
-      obj = TaxJp::Income.new(value)
-      expect(obj.tax_rate).to eq tax_rate
-      expect(obj.deductions).to eq deductions
-      expect(obj.tax_value).to eq value * tax_rate
+  test_data.each do |value, tax_rate, deductions|
+    context value do
+      let(:obj) {TaxJp::Income.new(value)}
+
+      describe "#tax_rate" do
+        example do
+          expect(obj.tax_rate).to eq tax_rate
+        end
+      end
+
+      describe "#tax_value" do
+        example do
+          expect(obj.tax_value).to eq value * tax_rate
+        end
+      end
+
+      describe "#deductions" do
+        example do
+          expect(obj.deductions).to eq deductions
+        end
+      end
+
     end
   end
 end
